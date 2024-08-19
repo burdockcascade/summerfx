8df6e6c79ba3913845d9ecd8d813b713dcc2da2d
Peter Richmond <peter.richmond@outlook.com>
2024-08-19 18:36:39 +0100

    vector3 and matrix structs

--- src/modules/modules.hpp
+++ src/modules/modules.hpp
@@ -9,5 +9,7 @@ void bind_window_module(lua_State *L);
 void bind_graphics2d_module(lua_State *L);
 
 void l_Vector2_push(lua_State *L, const Vector2 &vector2);
+void l_Vector3_push(lua_State *L, const Vector3 &vector3);
+void l_Matrix_push(lua_State *L, const Matrix &matrix);
 void l_Rectangle_push(lua_State *L, const Rectangle &rectangle);
 void l_Color_push(lua_State *L, const Color &color);
--- src/modules/structs.cpp
+++ src/modules/structs.cpp
@@ -136,14 +136,13 @@ int l_Vector2_newindex(lua_State *L) {
 
 	if (strcmp(key, "x") == 0) {
 		udata->x = luaL_checknumber(L, 1);
-		return 0;
-	}
-	if (strcmp(key, "y") == 0) {
+	} else if (strcmp(key, "y") == 0) {
 		udata->y = luaL_checknumber(L, 2);
-		return 0;
+	} else {
+		luaL_error(L, "Unknown field '%s' on Vector2", key);
 	}
 
-	return 1;
+	return 0;
 }
 
 void l_Vector2_push(lua_State *L, const Vector2 &vector2) {
@@ -161,6 +160,249 @@ void l_Vector2_push(lua_State *L, const Vector2 &vector2) {
 
 	// Set newindex function
 	lua_pushcfunction(L, l_Vector2_newindex);
+	lua_setfield(L, -2, "__newindex");
+
+	lua_setmetatable(L, -2);
+
+}
+
+// =====================================================================
+// Vector3
+
+// Vector3
+// --@table Vector3
+// --@desc A 3D vector
+// --@field x number
+// --@field y number
+// --@field z number
+int l_Vector3_constructor(lua_State *L) {
+
+	// Create struct
+	Vector3 s;
+	s.x = luaL_checknumber(L, 1);
+	s.y = luaL_checknumber(L, 2);
+	s.z = luaL_checknumber(L, 3);
+
+	// Push struct onto stack
+	l_Vector3_push(L, s);
+
+	return 1;
+}
+
+int l_Vector3_index(lua_State *L) {
+
+	Vector3 *udata = (Vector3*)luaL_checkudata(L, 1, "Vector3");
+	const char *key = luaL_checkstring(L, 2);
+
+	// Vector x component
+	if (strcmp(key, "x") == 0) {
+		lua_pushnumber(L, udata->x);
+	} else if (strcmp(key, "y") == 0) {
+		lua_pushnumber(L, udata->y);
+	} else if (strcmp(key, "z") == 0) {
+		lua_pushnumber(L, udata->z);
+	} else {
+		luaL_error(L, "Unknown field '%s' on ", key);
+	}
+
+	return 1;
+}
+
+int l_Vector3_newindex(lua_State *L) {
+
+	Vector3 *udata = (Vector3*)luaL_checkudata(L, 1, "Vector3");
+	const char *key = luaL_checkstring(L, 2);
+
+	if (strcmp(key, "x") == 0) {
+		udata->x = luaL_checknumber(L, 1);
+	}else if (strcmp(key, "y") == 0) {
+		udata->y = luaL_checknumber(L, 2);
+	} else if (strcmp(key, "z") == 0) {
+		udata->z = luaL_checknumber(L, 3);
+	} else {
+		luaL_error(L, "Unknown field '%s' on Vector3", key);
+	}
+
+	return 0;
+}
+
+void l_Vector3_push(lua_State *L, const Vector3 &vector3) {
+
+	// Create new user data
+	Vector3 *ud = (Vector3*)lua_newuserdata(L, sizeof(Vector3));
+	*ud = vector3;
+
+	// Set metatable
+	luaL_newmetatable(L, "Vector3");
+
+	// Set index function
+	lua_pushcfunction(L, l_Vector3_index);
+	lua_setfield(L, -2, "__index");
+
+	// Set newindex function
+	lua_pushcfunction(L, l_Vector3_newindex);
+	lua_setfield(L, -2, "__newindex");
+
+	lua_setmetatable(L, -2);
+
+}
+
+// =====================================================================
+// Matrix
+
+// Matrix
+// --@table Matrix
+// --@desc A 4x4 matrix
+// --@field m0 number
+// --@field m1 number
+// --@field m2 number
+// --@field m3 number
+// --@field m4 number
+// --@field m5 number
+// --@field m6 number
+// --@field m7 number
+// --@field m8 number
+// --@field m9 number
+// --@field m10 number
+// --@field m11 number
+// --@field m12 number
+// --@field m13 number
+// --@field m14 number
+// --@field m15 number
+int l_Matrix_constructor(lua_State *L) {
+
+	// Create struct
+	Matrix s;
+	s.m0 = luaL_checknumber(L, 1);
+	s.m1 = luaL_checknumber(L, 2);
+	s.m2 = luaL_checknumber(L, 3);
+	s.m3 = luaL_checknumber(L, 4);
+	s.m4 = luaL_checknumber(L, 5);
+	s.m5 = luaL_checknumber(L, 6);
+	s.m6 = luaL_checknumber(L, 7);
+	s.m7 = luaL_checknumber(L, 8);
+	s.m8 = luaL_checknumber(L, 9);
+	s.m9 = luaL_checknumber(L, 10);
+	s.m10 = luaL_checknumber(L, 11);
+	s.m11 = luaL_checknumber(L, 12);
+	s.m12 = luaL_checknumber(L, 13);
+	s.m13 = luaL_checknumber(L, 14);
+	s.m14 = luaL_checknumber(L, 15);
+	s.m15 = luaL_checknumber(L, 16);
+
+	// Push struct onto stack
+	l_Matrix_push(L, s);
+
+	return 1;
+}
+
+// Matrix field getter
+int l_Matrix_index(lua_State *L) {
+
+	Matrix *udata = (Matrix*)luaL_checkudata(L, 1, "Matrix");
+	const char *key = luaL_checkstring(L, 2);
+
+	// Matrix x component
+	if (strcmp(key, "m0") == 0) {
+		lua_pushnumber(L, udata->m0);
+	} else if (strcmp(key, "m1") == 0) {
+		lua_pushnumber(L, udata->m1);
+	} else if (strcmp(key, "m2") == 0) {
+		lua_pushnumber(L, udata->m2);
+	} else if (strcmp(key, "m3") == 0) {
+		lua_pushnumber(L, udata->m3);
+	} else if (strcmp(key, "m4") == 0) {
+		lua_pushnumber(L, udata->m4);
+	} else if (strcmp(key, "m5") == 0) {
+		lua_pushnumber(L, udata->m5);
+	} else if (strcmp(key, "m6") == 0) {
+		lua_pushnumber(L, udata->m6);
+	} else if (strcmp(key, "m7") == 0) {
+		lua_pushnumber(L, udata->m7);
+	} else if (strcmp(key, "m8") == 0) {
+		lua_pushnumber(L, udata->m8);
+	} else if (strcmp(key, "m9") == 0) {
+		lua_pushnumber(L, udata->m9);
+	} else if (strcmp(key, "m10") == 0) {
+		lua_pushnumber(L, udata->m10);
+	} else if (strcmp(key, "m11") == 0) {
+		lua_pushnumber(L, udata->m11);
+	} else if (strcmp(key, "m12") == 0) {
+		lua_pushnumber(L, udata->m12);
+	} else if (strcmp(key, "m13") == 0) {
+		lua_pushnumber(L, udata->m13);
+	} else if (strcmp(key, "m14") == 0) {
+		lua_pushnumber(L, udata->m14);
+	} else if (strcmp(key, "m15") == 0) {
+		lua_pushnumber(L, udata->m15);
+	} else {
+		luaL_error(L, "Unknown field '%s' on Matrix", key);
+	}
+
+	return 1;
+}
+
+// Matrix field setter
+int l_Matrix_newindex(lua_State *L) {
+
+	Matrix *udata = (Matrix*)luaL_checkudata(L, 1, "Matrix");
+	const char *key = luaL_checkstring(L, 2);
+
+	if (strcmp(key, "m0") == 0) {
+		udata->m0 = luaL_checknumber(L, 1);
+	} else if (strcmp(key, "m1") == 0) {
+		udata->m1 = luaL_checknumber(L, 2);
+	} else if (strcmp(key, "m2") == 0) {
+		udata->m2 = luaL_checknumber(L, 3);
+	} else if (strcmp(key, "m3") == 0) {
+		udata->m3 = luaL_checknumber(L, 4);
+	} else if (strcmp(key, "m4") == 0) {
+		udata->m4 = luaL_checknumber(L, 5);
+	} else if (strcmp(key, "m5") == 0) {
+		udata->m5 = luaL_checknumber(L, 6);
+	} else if (strcmp(key, "m6") == 0) {
+		udata->m6 = luaL_checknumber(L, 7);
+	} else if (strcmp(key, "m7") == 0) {
+		udata->m7 = luaL_checknumber(L, 8);
+	} else if (strcmp(key, "m8") == 0) {
+		udata->m8 = luaL_checknumber(L, 9);
+	} else if (strcmp(key, "m9") == 0) {
+		udata->m9 = luaL_checknumber(L, 10);
+	} else if (strcmp(key, "m10") == 0) {
+		udata->m10 = luaL_checknumber(L, 11);
+	} else if (strcmp(key, "m11") == 0) {
+		udata->m11 = luaL_checknumber(L, 12);
+	} else if (strcmp(key, "m12") == 0) {
+		udata->m12 = luaL_checknumber(L, 13);
+	} else if (strcmp(key, "m13") == 0) {
+		udata->m13 = luaL_checknumber(L, 14);
+	} else if (strcmp(key, "m14") == 0) {
+		udata->m14 = luaL_checknumber(L, 15);
+	} else if (strcmp(key, "m15") == 0) {
+		udata->m15 = luaL_checknumber(L, 16);
+	} else {
+		luaL_error(L, "Unknown field '%s' on Matrix", key);
+	}
+
+	return 0;
+}
+
+// Pushes a Matrix onto the stack
+void l_Matrix_push(lua_State *L, const Matrix &matrix) {
+
+	// Create new user data
+	Matrix *ud = (Matrix*)lua_newuserdata(L, sizeof(Matrix));
+	*ud = matrix;
+
+	// Set metatable
+	luaL_newmetatable(L, "Matrix");
+
+	// Set index function
+	lua_pushcfunction(L, l_Matrix_index);
+	lua_setfield(L, -2, "__index");
+
+	// Set newindex function
+	lua_pushcfunction(L, l_Matrix_newindex);
 	lua_setfield(L, -2, "__newindex");
 
 	lua_setmetatable(L, -2);
