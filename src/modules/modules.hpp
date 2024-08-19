783dd011885c961bfb0ca39a2483e480dc9063a1
Peter Richmond <peter.richmond@outlook.com>
2024-08-19 19:36:38 +0100

    fix structs

--- src/modules/modules.hpp
+++ src/modules/modules.hpp
@@ -10,6 +10,8 @@ void bind_graphics2d_module(lua_State *L);
 
 void l_Vector2_push(lua_State *L, const Vector2 &vector2);
 void l_Vector3_push(lua_State *L, const Vector3 &vector3);
+void l_Vector4_push(lua_State *L, const Vector4 &vector4);
 void l_Matrix_push(lua_State *L, const Matrix &matrix);
 void l_Rectangle_push(lua_State *L, const Rectangle &rectangle);
 void l_Color_push(lua_State *L, const Color &color);
+void l_Camera2D_push(lua_State *L, const Camera2D &camera2d);
